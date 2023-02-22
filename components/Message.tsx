df0cda3eaa8acdec82641edd7bf6a370e3fcdc85
IvanRavic88 <ravic.ivan88@gmail.com>
2023-02-22 13:37:00 +0100

    Added function Typing text

--- components/Message.tsx
+++ components/Message.tsx
@@ -1,4 +1,5 @@
 import { DocumentData } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
 type Props = {
   message: DocumentData;
@@ -6,12 +7,26 @@ type Props = {
 
 function Message({ message }: Props) {
   const isChatGTP = message.user.name === "ChatGPT";
+  const [typedText, setTypedText] = useState("");
+
+  useEffect(() => {
+    let index = 0;
+    const interval = setInterval(() => {
+      if (index < message.text.length) {
+        setTypedText((prevText) => prevText + message.text.charAt(index));
+        index++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 0);
+    return () => clearInterval(interval);
+  }, [message.text]);
 
   return (
     <div className={`py-5 text-white ${isChatGTP && "bg-[#434654]"}`}>
-      <div className="flex space-x-5 px-10 max-w-2xl mx-auto">
+      <div className="flex space-x-5 px-10 max-w-2xl ml-2">
         <img src={message.user.avatar} alt="" className="h-8 w-8" />
-        <p className="pt-1 text-sm">{message.text}</p>
+        <p className="pt-1 text-sm">{isChatGTP ? typedText : message.text}</p>
       </div>
     </div>
   );
